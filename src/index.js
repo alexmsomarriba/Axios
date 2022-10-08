7f3ad8acfb1d91cc1f01d7c75bf8041d8105eff2
Alex Somarriba <alex@teambond.io>
2022-10-08 13:52:42 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// import axios here
+const axios = require('axios')
+
+// Please note that it is normally not considered best practice to commit 
+// api keys to github as it presents a security risk. It is done here only 
+// for practice purposes as we are sharing the same account
+const api_key = 'd771b19ef336ed8381def3a60b574464'
+
+const discoverMovie = () => {
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
+  // code here
+  return axios(url)
+}
+
+const getMovieById = (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
+  // code here
+// axios(url).then(result => console.log(result.data))
+return(axios(url).then(result => result.data))
+}
+
+const getMovieByIdFailure = () => {
+  const fakeId = 5783 // FAKE ID HERE
+  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
+  // code here
+  return axios(url)
+    .then(result => result)
+    .catch( (err)=>err.response.status)
+}
+getMovieByIdFailure()
+
+
+module.exports = {
+  discoverMovie,
+  getMovieById,
+  getMovieByIdFailure
+}
